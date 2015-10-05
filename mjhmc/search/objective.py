56763afdafda6a64964c16970c3215cb66fc16a8
Andrew Berger <andberger@berkeley.edu>
2015-10-05 16:33:28 -0700

    unrolled helper method for obj_func

--- mjhmc/search/objective.py
+++ mjhmc/search/objective.py
@@ -5,7 +5,7 @@ from mjhmc.misc.plotting import plot_fit, plot_search_ac
 
 grad_evals = {
     'Gaussian' : int(2E5),
-    'RoughWell' : int(1E3),
+    'RoughWell' : int(5E4),
     'MultimodalGaussian' : int(2E5)
 }
 
@@ -13,7 +13,30 @@ debug = True
 
 
 def obj_func(sampler, distr, job_id, **kwargs):
-    num_target_grad_evals =  grad_evals[type(distr).__name__]
+    """ Scores the performance of sampler on distribution given parameters
+
+    :param sampler: sampler being tested. instance of mjhmc.samplers.markov_jump_hmc.HMCBase
+    :param distr: distribution being used. instance of mjhmc.misc.distributions.Distribution
+    :param job_id: integer label for job being run
+    :returns: the score
+    :rtype: float
+    """
+    cos_coef, normed_n_grad_evals, exp_coef, autocor, kwargs = obj_func_helper(sampler, distr, kwargs)
+    if debug:
+        plot_fit(normed_n_grad_evals, autocor, exp_coef, cos_coef, job_id, kwargs)
+    return exp_coef
+
+def obj_func_helper(sampler, distr, kwargs):
+    """ Helper function for the objective function
+
+    :param sampler: sampler being tested. instance of mjhmc.samplers.markov_jump_hmc.HMCBase
+    :param distr: distribution being used. instance of mjhmc.misc.distributions.Distribution
+    :param kwargs: dictionary of kwargs passed from parent function
+    :returns: parameters of fitted curves, graph data for computed autocorrelation, kwargs
+    :rtype: tuple
+
+    """
+     num_target_grad_evals =  grad_evals[type(distr).__name__]
     default_args = {
         "num_grad_steps": num_target_grad_evals,
         "sample_steps": 1,
@@ -29,11 +52,7 @@ def obj_func(sampler, distr, job_id, **kwargs):
     normed_n_grad_evals = n_grad_evals / (0.5 * num_target_grad_evals)
     autocor = ac_df['autocorrelation'].values
     exp_coef, cos_coef = fit(normed_n_grad_evals.copy(), autocor.copy())
-    if debug:
-        plot_fit(normed_n_grad_evals, autocor, exp_coef, cos_coef, job_id, kwargs)
-    return exp_coef
-
-
+    return cos_coef, normed_n_grad_evals, exp_coef, autocor, kwargs
 
 
 def min_idx(ac_df, target):
