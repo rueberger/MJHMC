4d0831df2d2fecdab83e40d7b2798ba02913a2df
Andrew Berger <andberger@berkeley.edu>
2015-10-08 20:47:35 -0700

    fix

--- mjhmc/search/objective.py
+++ mjhmc/search/objective.py
@@ -15,7 +15,8 @@ from mjhmc.samplers.markov_jump_hmc import ContinuousTimeHMC
 grad_evals = {
     'Gaussian' : int(2E5),
     'RoughWell' : int(5E4),
-    'MultimodalGaussian' : int(2E5)
+    'MultimodalGaussian' : int(2E5),
+    'ProductOfT' : int(5E4)
 }
 
 debug = True
