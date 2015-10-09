4231daa3e2c51c8b35ad0f12414728efff02d1be
Andrew Berger <andberger@berkeley.edu>
2015-10-08 21:42:32 -0700

    search directories for poe

---
+++ mjhmc/search/MJHMC_poe_36/mjhmc_objective.py
@@ -0,0 +1,8 @@
+from mjhmc.search.objective import obj_func
+from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC
+from mjhmc.misc.distributions import ProductOfT 
+
+
+def main(job_id, params):
+    print "job id: {}, params: {}".format(job_id, params)
+    return obj_func(MarkovJumpHMC, ProductOfT(nbatch=200), job_id, **params)
---
+++ mjhmc/search/control_poe_36/mjhmc_objective.py
@@ -0,0 +1,8 @@
+from mjhmc.search.objective import obj_func
+from mjhmc.samplers.markov_jump_hmc import ControlHMC 
+from mjhmc.misc.distributions import ProductOfT 
+
+
+def main(job_id, params):
+    print "job id: {}, params: {}".format(job_id, params)
+    return obj_func(ControlHMC, ProductOfT(nbatch=200), job_id, **params)
