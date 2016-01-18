e32faa3081397c914082938e305845a1159c5141
Andrew Berger <andberger@berkeley.edu>
2016-01-17 19:27:48 -0800

    fixed some bugs, made the script a little more flexible

--- mjhmc/search/find_best_params.py
+++ mjhmc/search/find_best_params.py
@@ -12,31 +12,33 @@ import numpy as np
 import json
 
 
+# format is [search_directory, num_hyperparameters ]
 searches = [
-    ["control_mm_gauss", True],
-    ["control_log_gauss", True],
-    ["control_rw", True],
-    ["MJHMC_mm_gauss", True],
-    ["MJHMC_log_gauss", True],
-    ["MJHMC_rw", True],
-    ["LAHMC_mm_gauss", True],
-    ["MJHMC_poe_36", True],
-    ["control_poe_36", True]
+    ["control_mm_gauss", 3],
+    ["control_log_gauss", 3],
+    ["control_rw", 3],
+    ["MJHMC_mm_gauss", 3],
+    ["MJHMC_log_gauss", 3],
+    ["MJHMC_rw", 3],
+    ["LAHMC_mm_gauss", 4],
+    ["MJHMC_poe_36", 3],
+    ["control_poe_36", 3]
 ]
 
 
-def find(directory, lahmc=False):
-    """ finds all of the hyperparameters and their scores for a Spearmint search by scanning
-    through the output logs
+def find(directory, num_params=3):
+    """ finds all of the hyperparameters and their scores for a Spearmint search by scanning through
+    the output logs
 
     no gaurantees if you run this on a debug log; clean out the output directory before starting
     new experiments
 
     :param directory: the directory of the output logs to scan through
-    :param lahmc: boolean flag, true for lahmc
+    :param num_params: the number of hyperparameters. 3 for standard hmc, 4 for lahmc
     :returns: An array of valid hyperparameters and their scores
     :rtype: float32 array of shape (num_valid_parameters, num_parameters + 1)
             for lahmc, num_parameters = 4, for all other samplers num_parameters = 3
+
     """
     results = []
     files = glob.glob(directory + '*')
@@ -53,16 +55,14 @@ def find(directory, lahmc=False):
                 else:
                     continue
             if re.search('epsilon',line):
-                for _ in xrange(4):
+                for idx in xrange(1, num_params + 1):
                     split_line = line.split('array([')
                     if len(split_line) != 0:
-                        uncast_param = split_line[1].split('])')[0]
+                        uncast_param = split_line[idx].split('])')[0]
                         result.append(np.float32(uncast_param))
                     else:
                         continue
-                if lahmc:
-                    result.append(np.float32(line.split('\'num_look_ahead_steps\': ')[1].split(',')[0]))
-            if len(result) >= 4:
+            if len(result) == num_params + 1:
                 results.append(result)
 
     if len(results) != 0:
@@ -71,8 +71,7 @@ def find(directory, lahmc=False):
 def write_best(results, directory):
     # occurs when search is running sometimes
     results = results[results[:, -1] != 0]
-    min_idx = np.argmin(results[:,0])
-#    min_idx = np.argsort(results[:,0])[1]
+    min_idx = np.argmin(results[:, 0])
     best_par = results[min_idx]
     params = {
         'epsilon' : float(best_par[1]),
@@ -85,11 +84,10 @@ def write_best(results, directory):
         json.dump(params, d)
 
 def write_all():
-    for directory, flag in searches:
-        if flag == True:
-            results = find("{}/output/".format(directory), lahmc=False)
-            if results == None:
-                print "No output found for {}".format(directory)
-            else:
-                write_best(results, directory)
-                print "Parameters succesfully updated for {}".format(directory)
+    for directory, num_params in searches:
+        results = find("{}/output/".format(directory), num_params)
+        if results == None:
+            print "No output found for {}".format(directory)
+        else:
+            write_best(results, directory)
+            print "Parameters succesfully updated for {}".format(directory)
