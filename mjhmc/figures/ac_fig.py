e0106ab25d4ecb8d9217afc6d035b624b58be2cb
Andrew Berger <andberger@berkeley.edu>
2016-01-29 18:57:45 -0800

    format nicer

--- mjhmc/figures/ac_fig.py
+++ mjhmc/figures/ac_fig.py
@@ -88,7 +88,7 @@ def plot_ac(distribution, control_params, mjhmc_params, lahmc_params, max_steps=
     plt.title("{}D {}".format(distribution.ndims, type(distribution).__name__))
     plt.legend()
     plt.show()
-    plt.savefig("{}_{}_dim__ac.pdf".format(type(distribution).__name__, distribution.ndims))
+    plt.savefig("{}_{}_dim_ac.pdf".format(type(distribution).__name__, distribution.ndims))
     plt.show()
 
 def load_params(distribution):
