127260bcca52556aba8bc921f77d79f1bd919e0e
Andrew Berger <andberger@berkeley.edu>
2016-02-04 22:51:07 -0800

    new test cases

--- mjhmc/tests/test_fast_ac.py
+++ mjhmc/tests/test_fast_ac.py
@@ -4,8 +4,8 @@ This module contains unit tests for the theano autocorrelation function
 import unittest
 import numpy as np
 
-from mjhmc.fast.hmc import normed_autocorrelation
-from mjhmc.misc.autocor import slow_autocorrelation, sample_to_df
+
+from mjhmc.misc.autocor import autocorrelation, slow_autocorrelation, sample_to_df
 
 from mjhmc.misc.distributions import Gaussian, MultimodalGaussian
 from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC
@@ -20,19 +20,38 @@ class TestFastAutocorrelation(unittest.TestCase):
         np.random.seed(2015)
 
 
-    def test_autocorrelation_good_init(self):
+    def test_autocorrelation_good_init_full_window(self):
         """ Tests that the legacy and fast ac implementations produce identical output
         when the sampler is not initialized in a biased manner
         (meaning we don't have to worry about variance mismatch)
+        runs on the full window
 
         :returns: None
         :rtype: None
         """
         gaussian = Gaussian()
         sample_df = sample_to_df(MarkovJumpHMC, gaussian, num_steps=1000)
-        slow_ac_df = slow_autocorrelation(sample_df)
+        slow_ac_df = slow_autocorrelation(sample_df, half_window=False)
         slow_ac = slow_ac_df.autocorrelation.as_matrix()
-        fast_ac_df = normed_autocorrelation(sample_df)
+        fast_ac_df = autocorrelation(sample_df, half_window=False)
+        fast_ac = fast_ac_df.autocorrelation.as_matrix()
+        self.assertTrue(np.isclose(slow_ac, fast_ac).all())
+
+
+    def test_autocorrelation_good_init_half_window(self):
+        """ Tests that the legacy and fast ac implementations produce identical output
+        when the sampler is not initialized in a biased manner
+        (meaning we don't have to worry about variance mismatch)
+        runs on the half window
+
+        :returns: None
+        :rtype: None
+        """
+        gaussian = Gaussian()
+        sample_df = sample_to_df(MarkovJumpHMC, gaussian, num_steps=1000)
+        slow_ac_df = slow_autocorrelation(sample_df, half_window=True)
+        slow_ac = slow_ac_df.autocorrelation.as_matrix()
+        fast_ac_df = autocorrelation(sample_df, half_window=True)
         fast_ac = fast_ac_df.autocorrelation.as_matrix()
         self.assertTrue(np.isclose(slow_ac, fast_ac).all())
 
@@ -48,6 +67,6 @@ class TestFastAutocorrelation(unittest.TestCase):
         sample_df = sample_to_df(MarkovJumpHMC, gaussian, num_steps=1000)
         slow_ac_df = slow_autocorrelation(sample_df)
         slow_ac = slow_ac_df.autocorrelation.as_matrix()
-        fast_ac_df = normed_autocorrelation(sample_df)
+        fast_ac_df = autocorrelation(sample_df)
         fast_ac = fast_ac_df.autocorrelation.as_matrix()
         self.assertTrue(np.isclose(slow_ac, fast_ac).all())
