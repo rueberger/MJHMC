e5d9c120dc495a757adbf8201cf8dc1578eeed0c
Andrew Berger <andberger@berkeley.edu>
2016-02-04 21:10:58 -0800

    another fix for the test was testing for exact floating point equality, now checks within epsillon

--- mjhmc/tests/test_fast_ac.py
+++ mjhmc/tests/test_fast_ac.py
@@ -28,6 +28,8 @@ class TestFastAutocorrelation(unittest.TestCase):
         """
         gaussian = Gaussian()
         sample_df = sample_to_df(MarkovJumpHMC, gaussian, num_steps=1000)
-        slow_ac = autocorrelation(sample_df)
-        fast_ac = normed_autocorrelation(sample_df)
-        self.assertTrue((slow_ac.autocorrelation.as_matrix() == fast_ac.autocorrelation.as_matrix()).all())
+        slow_ac_df = autocorrelation(sample_df)
+        slow_ac = slow_ac_df.autocorrelation.as_matrix()
+        fast_ac_df = normed_autocorrelation(sample_df)
+        fast_ac = fast_ac_df.autocorrelation.as_matrix()
+        self.assertTrue(np.isclose(slow_ac, fast_ac))
