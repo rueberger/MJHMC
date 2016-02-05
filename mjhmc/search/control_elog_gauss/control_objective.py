20801e838dd99c2fee8baaf0ca41e6f31642ff28
Andrew Berger <andberger@berkeley.edu>
2016-02-05 07:38:34 -0800

    updating ill-conditioned gaussian for search importantly, set a fixed random seed in each objective function which wil ensure that both searches are identically initialized

--- mjhmc/search/control_elog_gauss/control_objective.py
+++ mjhmc/search/control_elog_gauss/control_objective.py
@@ -1,9 +1,11 @@
+import numpy as np
 from search.objective import obj_func
 from samplers.markov_jump_hmc import ControlHMC
 from misc.distributions import Gaussian
 
+np.random.seed(2016)
 
 def main(job_id, params):
     print "job id: {}, params: {}".format(job_id, params)
-    return obj_func(ControlHMC, Gaussian(ndims=50, nbatch=200, log_conditioning=2),
+    return obj_func(ControlHMC, Gaussian(ndims=50, nbatch=50, log_conditioning=1.1),
                     job_id, **params)
