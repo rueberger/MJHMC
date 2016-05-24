1759d8e4ca58bb1a3e560c98d5bf63ab140e8304
Andrew Berger <andberger@berkeley.edu>
2016-05-23 18:27:59 -0700

    added a benchmark that gives the average time per sample for various samplers in different configurations

--- mjhmc/misc/benchmarks.py
+++ mjhmc/misc/benchmarks.py
@@ -8,7 +8,7 @@ plt.ion()
 import time
 
 
-from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC
+from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC. ControlHMC
 from mjhmc.misc.distributions import ProductOfT, Gaussian
 from mjhmc.misc.autocor import calculate_autocorrelation
 
@@ -125,3 +125,54 @@ def autocorrelation_vs_n_samples(sampler_cls=MarkovJumpHMC,
     axis.set_ylabel("log seconds per sample")
     axis.set_xlabel("batch size")
     axis.set_title("{}: autocorrelation compute time by number of samples".format(sampler_cls.__name__))
+
+def time_per_sample(sampler, trials=10, **kwargs):
+    """ Helper function. Computes average time per sample
+
+    :param trials: number of trials to average over
+    :param sampler: initialized sampler to test
+    :returns: the average time per sample
+    :rtype: float
+    """
+    N_SAMPLES = 1000
+    times = np.zeros(trials)
+    for tr_idx in xrange(trials):
+        t_i = time.time()
+        sampler.sample(nsamples=N_SAMPLES, **kwargs)
+        t_f = time.time()
+        times[tr_idx] = t_f - t_i
+    return np.mean(times)
+
+def sampler_speedometer():
+    """ Tests average sample speed of different samplers in different configurations
+
+    :returns: Just prints info
+    :rtype: None
+    """
+
+    gaussian = Gaussian()
+    np.random.seed(2015)
+    pot = ProductOfT(ndims=36,nbasis=36)
+    mjhmc_gauss = MarkovJumpHMC(distribution=gaussian)
+    mjhmc_gauss_nr = MarkovJumpHMC(distribution=gaussian, resample=False)
+    control_gauss = ControlHMC(distribution=gaussian)
+    mjhmc_pot = MarkovJumpHMC(distribution=pot)
+    mjhmc_pot_nr = MarkovJumpHMC(distribution=pot, resample=False)
+    control_pot = ControlHMC(distribution=pot)
+
+    m_g_r_avg = time_per_sample(mjhmc_gauss)
+    m_g_nr_avg = time_per_sample(mjhmc_gauss_nr)
+    c_g_avg = time_per_sample(control_gauss)
+
+    m_p_r_avg = time_per_sample(mjhmc_pot)
+    m_p_nr_avg = time_per_sample(mjhmc_pot_nr)
+    c_p_avg = time_per_sample(control_pot)
+
+    print "Average times per samples..."
+    print "resampled MJHMC numpy gradient: {}".format(m_g_r_avg)
+    print "not resampled MJHMC numpy gradient: {}".format(m_g_nr_avg)
+    print "control HMC numpy gradient: {}".format(c_g_avg)
+
+    print "resampled MJHMC theano gradient: {}".format(m_p_r_avg)
+    print "not resampled MJHMC theano gradient: {}".format(m_p_nr_avg)
+    print "control HMC theano gradient: {}".format(c_p_avg)
