64314883c474e63516bd15cd7db55e2a37415261
Andrew Berger <andberger@berkeley.edu>
2016-07-20 13:52:22 -0400

    search configurations for funnel

---
+++ mjhmc/search/control_funnel/control_objective.py
@@ -0,0 +1,7 @@
+from mjhmc.search.objective import obj_func
+from mjhmc.samplers.markov_jump_hmc import ControlHMC
+from mjhmc.misc.distributions import Funnel
+
+def main(job_id, params):
+    print "job id: {}, params: {}".format(job_id, params)
+    return obj_func(ControlHMC, Funnel(nbatch=1000), job_id,  **params)
