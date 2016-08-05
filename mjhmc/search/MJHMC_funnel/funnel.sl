65755daea28cf8d6aed1aad9c3957f11605d4f1b
Mayur Mudigonda <mayur.mudigonda@gmail.com>
2016-08-05 16:11:23 -0700

    Added the slurm file that submits the jobs on the cluster. Also fixed the objective.py to include the Funnel distribution

--- mjhmc/search/MJHMC_funnel/funnel.sl
+++ mjhmc/search/MJHMC_funnel/funnel.sl
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #SBATCH -p regular
-#SBATCH -N 10
-#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH -t 0:10:00
 #SBATCH -L SCRATCH
 
 module load spearmint
--- mjhmc/search/objective.py
+++ mjhmc/search/objective.py
@@ -16,7 +16,8 @@ grad_evals = {
     'Gaussian' : int(5E4),
     'RoughWell' : int(5E4),
     'MultimodalGaussian' : int(2E5),
-    'ProductOfT' : int(1E5)
+    'ProductOfT' : int(1E5),
+    'Funnel': int(1E5)
 }
 
 debug = True
