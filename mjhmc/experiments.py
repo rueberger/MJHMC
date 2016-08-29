3b58f4e46d22219681bbdea32729c04a182d0532
Andrew Berger <andberger@berkeley.edu>
2016-08-28 23:05:23 -0700

    updated method to get numerical errs of ladder

--- mjhmc/experiments.py
+++ mjhmc/experiments.py
@@ -7,9 +7,10 @@ from mjhmc.samplers.markov_jump_hmc import ControlHMC
 from mjhmc.misc.distributions import Gaussian
 
 
-def numerical_error_distr(distr=None, n_batch=5000, n_steps=int(1e4)):
+def ladder_numerical_err_hist(distr=None, n_steps=int(1e5)):
     """ Compute a histogram of the numerical integration error
-     of a particle on the ladder.
+    on the state ladder. Implicitly assumes that such a distribution exists
+    and is shared by all ladders
 
     Args:
      distr: distribution object to run on, make sure n_batch is big
@@ -17,11 +18,22 @@ def numerical_error_distr(distr=None, n_batch=5000, n_steps=int(1e4)):
     Returns:
      energies = {E(L^j \zeta) : j \in {0, ..., k}}^{n_batch}
     """
-    distr = distr or Gaussian(nbatch=n_batch)
+    distr = distr or Gaussian(nbatch=1)
     sampler = ControlHMC(distribution=distr)
-    state = sampler.state
-    energies = np.zeros((n_steps, n_batch))
-    for step_idx in range(n_steps):
-        energies[step_idx, :] = np.squeeze(state.H())
-    energies -= energies[0, :]
-    return energies
+
+    # [[ladder_energies]]
+    energies = []
+    r_counts = [0]
+    ladder_energies = [np.squeeze(sampler.state.H())]
+    for _ in range(n_steps):
+        if sampler.r_count == r_counts[-1]:
+            ladder_energies.append(np.squeeze(sampler.state.H()))
+        else:
+            energies.append(np.array(ladder_energies))
+            ladder_energies = [np.squeeze(sampler.state.H())]
+        r_counts.append(sampler.r_count)
+        sampler.sampling_iteration()
+    centered_energies = []
+    for ladder_energies in energies:
+        centered_energies += list(ladder_energies - ladder_energies[0])
+    return centered_energies
