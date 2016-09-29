893c425c4a6a26c50914f69fe2542a8063e672c0
Andrew Berger <andberger@berkeley.edu>
2016-09-29 16:58:44 -0400

    added initializers for pot params

--- mjhmc/search/MJHMC_poe_36/mjhmc_objective.py
+++ mjhmc/search/MJHMC_poe_36/mjhmc_objective.py
@@ -4,11 +4,20 @@ from mjhmc.misc.distributions import ProductOfT
 from scipy.sparse import rand
 import numpy as np
 
-np.random.seed(2015)
-
 
 def main(job_id, params):
     ndims = 36
     nbasis = 36
     print "job id: {}, params: {}".format(job_id, params)
-    return obj_func(MarkovJumpHMC, ProductOfT(nbatch=25,ndims=ndims,nbasis=nbasis), job_id, **params)
+    weights, lognu = init_weights(ndims, nbasis)
+    return obj_func(MarkovJumpHMC, ProductOfT(nbatch=25, ndims=ndims, nbasis=nbasis, lognu=lognu, W=weights), job_id, **params)
+
+def init_weights(ndims, nbasis):
+    """ Sparse normal weights,
+    """
+    np.random.seed(2015)
+    sp_var = np.random.rand(ndims, nbasis)
+    w_sp = np.random.randn(ndims, nbasis)
+    w_sp[sp_var > 0.05] = 0
+    lognu = np.log(np.random.rand(nbasis,) * 2 + 2.1)
+    return w_sp, lognu
