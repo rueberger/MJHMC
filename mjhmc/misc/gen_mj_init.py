6677edad6be7446a9a910876e44298c0f6b15a78
Andrew Berger <andberger@berkeley.edu>
2016-11-22 20:27:49 -0500

    training wheels off

--- mjhmc/misc/gen_mj_init.py
+++ mjhmc/misc/gen_mj_init.py
@@ -7,10 +7,8 @@ import numpy as np
 from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC, ControlHMC
 from .utils import package_path
 
-BURN_IN_STEPS = 1000
-VAR_STEPS = 500
-# BURN_IN_STEPS = int(2E6)
-# VAR_STEPS = int(1E6)
+BURN_IN_STEPS = int(2E6)
+VAR_STEPS = int(1E6)
 MAX_N_PARTICLES = 1000
 
 def generate_initialization(distribution):
