56e3eafb57677288ea341abdab5a6e7e8df66a57
Andrew Berger <andberger@berkeley.edu>
2016-12-23 21:01:28 -0800

    wip ladder table insert, init, util methods

--- mjhmc/figures/sg_fig.py
+++ mjhmc/figures/sg_fig.py
@@ -7,6 +7,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import seaborn as sns
+import tables
 
 import numpy as np
 from scipy.linalg import eig
@@ -25,7 +26,6 @@ from mjhmc.experiments.spectral import fit_inv_pdf, ladder_numerical_err_hist
 sns.set_palette("cubehelix", n_colors=2)
 sns.set_context("talk")
 sns.set_style("whitegrid", {"axes.linewidth": .5})
-
 
 def sg(algebraic_sampler, full):
     """
@@ -102,3 +102,49 @@ def plot_spectral_gaps(max_n_dims, n_trials=25,
     else:
         plt.savefig("{}/sg_gap_half_{}_energies_{}_trials.pdf".format(
             expanduser(save_directory), max_n_dims, n_trials))
+
+def generate_sp_img_ladders(max_steps=int(1e6)):
+    """ Run MJHMC and control for a while on sp_img, save the ladders
+    to the ladder table
+
+    Args:
+       max_steps: number of steps to run samples for - int
+
+    """
+    from mjhmc.figures.ac_fig import load_params
+    from mjhmc.misc.tf_distributions import SparseImageCode
+    from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC, ControlHMC
+
+    sp_img = SparseImageCode()
+
+    # open table, iterate through ladder gen append to table
+    # append ladder energies as groups with indices in metadata
+
+
+def init_ladder_table():
+    """ Create the table where ladders are stored
+    """
+    with tables.open_file(ladder_table_path(), mode='w', title='ladder_table') as l_file:
+        metadata_table = l_file.create_table('/', 'ladder_metdata', description=LadderTableSchema,
+                                           expected_rows=int(5e4))
+        ladder_group = l_file.create_group('/', 'ladders', 'ladder_energies')
+
+        # set metadata table indices
+        metadata_table.cols.epsilon.create_csindex()
+        metadata_table.cols.num_leapfrog_steps.create_csindex()
+        metadata_table.cols.beta.create_csindex()
+        metadata_table.cols.distr_hash.create_csindex()
+
+
+
+class LadderTableSchema(tables.IsDescription):
+    epsilon = tables.Float32Col()
+    num_leapfrog_steps = tables.Int32Col()
+    beta = tables.Float32Col()
+    ladder_idx = tables.Int32Col()
+    distr_hash = tables.Float32Col()
+    mjhmc = tables.BoolCol()
+
+def ladder_table_path():
+    from mjhmc.misc.utils import package_path
+    return "{}/distr_data/ladder_table.h5".format(package_path())
