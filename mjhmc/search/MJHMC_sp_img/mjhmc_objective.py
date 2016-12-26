fd439bef5a81a4d20181f3a3958e41f37d4056ce
Andrew Berger <andberger@berkeley.edu>
2016-12-26 12:15:35 -0800

    keyword error

--- mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
+++ mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
@@ -5,4 +5,4 @@ from mjhmc.misc.tf_distributions import SparseImageCode
 
 def main(job_id, params):
     print "job id: {}, params: {}".format(job_id, params)
-    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=9, nbatch=10), job_id, **params)
+    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=9, n_batches=10), job_id, **params)
