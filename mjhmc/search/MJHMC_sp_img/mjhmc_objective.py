bf965e1a0aea7b2e748353e62fe897ba87948bbf
Andrew Berger <andberger@berkeley.edu>
2016-12-26 12:31:43 -0800

    use half of gpu memory for search

--- mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
+++ mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
@@ -5,4 +5,5 @@ from mjhmc.misc.tf_distributions import SparseImageCode
 
 def main(job_id, params):
     print "job id: {}, params: {}".format(job_id, params)
-    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=9, n_batches=10), job_id, **params)
+    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=9, n_batches=10,
+                                                   device='/gpu:0', gpu_frac=0.5), job_id, **params)
--- mjhmc/search/control_sp_img/control_objective.py
+++ mjhmc/search/control_sp_img/control_objective.py
@@ -4,4 +4,5 @@ from mjhmc.misc.tf_distributions import SparseImageCode
 
 def main(job_id, params):
     print "job id: {}, params: {}".format(job_id, params)
-    return obj_func(ControlHMC, SparseImageCode(n_patches=9, n_batches=10), job_id,  **params)
+    return obj_func(ControlHMC, SparseImageCode(n_patches=9, n_batches=10,
+                                                device='/gpu:0', gpu_frac=0.5), job_id, **params)
