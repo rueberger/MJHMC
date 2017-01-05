0bf9686fdcd6c259e7bba0e67b5aaf5d9fb90664
Andrew Berger <andberger@berkeley.edu>
2017-01-05 15:45:25 -0700

    modified API to make samplers more accessible
    can now be reached at `mjhmc.MarkovJumpHMC` and `mjhmc.ControlHMC`

--- mjhmc/__init__.py
+++ mjhmc/__init__.py
@@ -3,7 +3,8 @@
 """
 __all__ = ['figures', 'misc', 'samplers', 'tests']
 
+from mjhmc.samplers.markov_jump_hmc import MarkovJumpHMC, ControlHMC
+
 import mjhmc.figures
 import mjhmc.misc
-import mjhmc.samplers
 import mjhmc.tests
