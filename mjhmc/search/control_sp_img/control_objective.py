3e0c59657fd69b41d23c33305287b986a79234b8
Andrew Berger <andberger@berkeley.edu>
2017-01-05 12:20:51 -0800

    benchmarks indicate it is optimal to run grad on cpu, energy on gpu

--- mjhmc/search/control_sp_img/control_objective.py
+++ mjhmc/search/control_sp_img/control_objective.py
@@ -4,5 +4,8 @@ from mjhmc.misc.tf_distributions import SparseImageCode
 
 def main(job_id, params):
     print "job id: {}, params: {}".format(job_id, params)
+    # counter-intuitively, benchmarks indicate that this is optimal
+    device_dict = {'grad': '/cpu:0',
+                   'energy': '/gpu:0'}
     return obj_func(ControlHMC, SparseImageCode(n_patches=9, n_batches=10,
-                                                device='/gpu:0', gpu_frac=1), job_id, **params)
+                                                device=device_dict, gpu_frac=1), job_id, **params)
