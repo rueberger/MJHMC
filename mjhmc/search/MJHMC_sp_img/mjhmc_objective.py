160495e381f49a9b8e1023d820a378c305607bfc
Andrew Berger <andberger@berkeley.edu>
2017-01-18 09:42:01 -0500

    use single patch for search, incremented search names

--- mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
+++ mjhmc/search/MJHMC_sp_img/mjhmc_objective.py
@@ -8,5 +8,5 @@ def main(job_id, params):
     # counter-intuitively, benchmarks indicate that this is optimal
     device_dict = {'grad': '/cpu:0',
                    'energy': '/gpu:0'}
-    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=9, n_batches=10,
+    return obj_func(MarkovJumpHMC, SparseImageCode(n_patches=1, n_batches=10,
                                                    device=device_dict, gpu_frac=1), job_id, **params)
--- mjhmc/search/control_sp_img/control_objective.py
+++ mjhmc/search/control_sp_img/control_objective.py
@@ -7,5 +7,5 @@ def main(job_id, params):
     # counter-intuitively, benchmarks indicate that this is optimal
     device_dict = {'grad': '/cpu:0',
                    'energy': '/gpu:0'}
-    return obj_func(ControlHMC, SparseImageCode(n_patches=9, n_batches=10,
+    return obj_func(ControlHMC, SparseImageCode(n_patches=1, n_batches=10,
                                                 device=device_dict, gpu_frac=1), job_id, **params)
